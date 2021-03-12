384ab52140c5fc45401f8d7ab7ce30719ad37850
Tarek Ahmed <tarek5701@gmail.com>
2021-03-12 00:15:03 -0800

    Login page style updated

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, message } from 'antd';
+import { Form, Input, Button, message, Card, Row, Col } from 'antd';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
@@ -38,45 +38,42 @@ export default function LoginPage({ isAuthenticated }) {
             }}
         />
     ) : (
-        <Form
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            style={{ width: '30%', margin: 'auto', marginTop: '10%' }}
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item>
-                <Button
-                    style={{ background: '#8f5db7', color: 'white',width:'40%',marginLeft:'30%' }}
-                    htmlType="submit"
-                >
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+        <div className="container my-5" style={{width:'80vw'}}>
+            <Card>
+                <Row justify="center" className="py-5">
+                    <Col xs={20} md={14}>
+                        <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your username!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button shape="round" size="large" type="primary" htmlType="submit" style={{marginLeft:"40%"}}>Login</Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>
+            </Card>
+        </div>
     );
 }
